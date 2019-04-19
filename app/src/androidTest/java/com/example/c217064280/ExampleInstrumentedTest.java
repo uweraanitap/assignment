55aaab8c60d7601ad99d210531d05a4f494b1a88
uweraanitap <49066524+uweraanitap@users.noreply.github.com>
2019-04-19 06:05:42 -0700

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/c217064280/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/c217064280/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.c217064280;
+package com.example.anitha;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.c217064280", appContext.getPackageName());
+        assertEquals("com.example.c217144411", appContext.getPackageName());
     }
 }
